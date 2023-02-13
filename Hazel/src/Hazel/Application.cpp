8c752c4b914d78c99b1136cd10f6c648fbf9cb5a
xuan <www416277970@qq.com>
2023-02-13 22:45:11 +0800

    事件系统代码

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,6 +1,9 @@
 #include "Application.h"
 #include <stdio.h>
 #include "Log.h"
+#include "Events/ApplicationEvent.h"
+#include "Events/MouseEvent.h"
+#include "Events/KeyEvent.h"
 namespace Hazel {
 	Application::Application() {
 
@@ -12,5 +15,12 @@ namespace Hazel {
 
 	void Application::Run() {
 		HZ_CLIENT_T("xuanTest hello");
+		HZ_CORE_I("–≈œ¢¥Ú”°");
+		auto wrEvent = WindowResizeEvent(100, 200);
+		HZ_CORE_I(wrEvent.ToString());
+		auto kEvent = KeyPressedEvent(1);
+		HZ_CORE_I(kEvent.ToString());
+		auto mEvent = MouseButtonPressedEvent(1);
+		HZ_CORE_I(mEvent.ToString());
 	}
 }
--- Hazel/src/Hazel/Core.h
+++ Hazel/src/Hazel/Core.h
@@ -10,3 +10,5 @@
 #else
 	#error Hazel not support	
 #endif
+
+#define BIT(x) (1 << x)
